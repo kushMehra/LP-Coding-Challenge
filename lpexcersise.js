330a5845198df5e07abf2d2d8071fae1c305c9f1
kushMehra <kushagra.mehra@gmail.com>
2021-12-10 06:36:32 +1100

    added account number handling

--- lpexcersise.js
+++ lpexcersise.js
@@ -4,66 +4,79 @@ let jwt = "";
 let conversationId = null;
 let messageId = 0;
 let connectionStatus = false;
-let accountNumber = '78099952';
+let accountNumber = "";
 var socket;
 
-document.getElementById("start").disabled = false;
+document.getElementById("getDomains").disabled = false;
+document.getElementById("start").disabled = true;
 document.getElementById("end").disabled = true;
  
 getdata = () => {
 
-    //get the idp domain info
-    fetch('https://api.liveperson.net/api/account/' + accountNumber + '/service/idp/baseURI.json?version=1.0')
-     .then(response => {
-        if (response.ok) {
-            return response.json();
-        } else {
-            throw new Error('Something went wrong');
-        }
-      }).then(data => {
-            idpDomain = data.baseURI;
-            document.getElementById('idpDomain').innerHTML = idpDomain;
-            console.log(idpDomain);
-
-            // if successful, retrieve the jwt that will be utilised to connect to the websocket
-            fetch('https://'+idpDomain+'/api/account/' + accountNumber + '/signup', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(""),
-                }).then(response => {
+    accountNumber = document.getElementById("account").value;
+    if (accountNumber === "") {
+        alert ("Please enter the account number");
+    }
+    else {
+        //get the idp domain info
+        fetch('https://api.liveperson.net/api/account/' + accountNumber + '/service/idp/baseURI.json?version=1.0')
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                alert ("Perhaps the account number is invalid. Please validate and enter the correct account number again.")
+                throw new Error('Something went wrong');
+            }
+        }).then(data => {
+                // get the asyncMessagingEnt domain to send messages    
+                fetch('https://api.liveperson.net/api/account/' + accountNumber + '/service/asyncMessagingEnt/baseURI.json?version=1.0')
+                .then(response => {
                     if (response.ok) {
                         return response.json();
                     } else {
                         throw new Error('Something went wrong');
                     }
                 }).then(data => {
-                    //save jwt to be used later for websocket connection
-                    jwt = data.jwt;
-                    console.log('Success:', jwt);
+                        msgDomain = data.baseURI;
+                        document.getElementById('msgDomain').innerHTML = msgDomain;
+                        console.log(msgDomain);
                     }).catch((error) => {
-                        console.error('Error:', error);
-                    });
-        }).catch((error) => {
-            console.log('Error:', error)
-        });
+                        console.log('Error:', error)
+                });
 
-    // get the asyncMessagingEnt domain to send messages    
-    fetch('https://api.liveperson.net/api/account/' + accountNumber + '/service/asyncMessagingEnt/baseURI.json?version=1.0')
-     .then(response => {
-        if (response.ok) {
-            return response.json();
-        } else {
-            throw new Error('Something went wrong');
-        }
-      }).then(data => {
-            msgDomain = data.baseURI;
-            document.getElementById('msgDomain').innerHTML = msgDomain;
-            console.log(msgDomain);
-        }).catch((error) => {
-            console.log('Error:', error)
-    });
+                idpDomain = data.baseURI;
+                document.getElementById('idpDomain').innerHTML = idpDomain;
+                console.log(idpDomain);
+
+                // if successful, retrieve the jwt that will be utilised to connect to the websocket
+                fetch('https://'+idpDomain+'/api/account/' + accountNumber + '/signup', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(""),
+                    }).then(response => {
+                        if (response.ok) {
+                            return response.json();
+                        } else {
+                            throw new Error('Something went wrong');
+                        }
+                    }).then(data => {
+                        //save jwt to be used later for websocket connection
+                        jwt = data.jwt;
+                        console.log('Success:', jwt);
+                        }).catch((error) => {
+                            console.error('Error:', error);
+                        });
+
+                        document.getElementById("getDomains").disabled = true;
+                        document.getElementById("start").disabled = false;
+                        document.getElementById("end").disabled = true;
+
+            }).catch((error) => {
+                console.log('Error:', error)
+            });
+    }
 }
 
 // establish the connection
@@ -138,6 +151,7 @@ disconnectSocket = () => {
     socket.send(JSON.stringify(payload));
     socket.close();
     document.getElementById('status').innerHTML = "Connection Disconnected";
+    document.getElementById("getDomains").disabled = false;
     document.getElementById("start").disabled = false;
     document.getElementById("end").disabled = true;
 }
